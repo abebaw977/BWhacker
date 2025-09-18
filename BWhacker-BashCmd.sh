a4043298f86b8030c1bfb52537705705da250f10
abebawmans@gmail.com <your@email.com>
2025-09-18 18:43:46 +0000

    Updated

---
+++ BWhacker-BashCmd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install -y nmap dnsutils curl traceroute whois python3-pip python3-venv git golang assetfinder dig 
+
+if ! command -v assetfinder &> /dev/null
+then
+    git clone https://github.com/tomnomnom/assetfinder.git ~/assetfinder
+    cd ~/assetfinder
+    go build
+    sudo mv assetfinder /usr/local/bin/
+    cd ~
+fi
+read -r input "Enter pythpn env: " envs
+python3 -m venv envs
+source envs/bin/activate
+
+pip install --upgrade pip
+pip install scapy abu_color==0.2 aiohttp pyperclip PyGetWindow psutil tqdm rich
+
+echo "Installation complete. Activate environment with: source bwhacker_venv/bin/activate"
